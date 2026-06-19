a745d3471105135d79ef6b37efd0e0b9bfc98a91
Prashant Mohite <prashant.mohite@kinetixtt.com>
2026-06-19 06:15:40 +0530

    added variable and data types practice

---
+++ Basics/input-practice.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Enter you name: ")
+	// name := ""
+	var name string
+	fmt.Scan(&name)
+
+	fmt.Printf("Hi %v, welecome to golang programming \n", name)
+}
---
+++ Basics/variales-data-types.go
@@ -0,0 +1,36 @@
+// package main
+
+// import "fmt"
+
+// func main() {
+// 	// declaring a variable with var keyword
+// 	var name string = "Palistha"
+// 	fmt.Println(name)
+
+// 	name2 := "prashant"
+
+// 	fmt.Printf("name = %v , name2 = %v", name, name2)
+
+// 	// data types in golang
+// 	var age int = 22
+// 	// or
+// 	age2 := 22
+
+// 	fmt.Printf("age = %v , age2 = %v", age, age2)
+
+// 	// float
+// 	marks := 95.5
+// 	fmt.Printf("marks =  %v \n", marks)
+
+// 	// constants in golang : Note we cant use shorthand declaration for constant variables
+// 	const m2 = 100
+
+// 	// we cant change the value of constant variable
+// 	// m2 = 200     // this will give an error
+// 	marks = 90.5 // this will not give an error
+// 	fmt.Printf("m2 = %v , marks = %v \n", m2, marks)
+
+// 	vegiterian := true
+// 	fmt.Printf("vegiterian = %v \n", vegiterian)
+
+// }
